e38d00c2c3e819bd3a2f8dc4b402d2fcd3ab082a
Gabriel Loch <grl.loch@gmail.com>
2021-07-24 14:35:53 -0300

    Dev

---
+++ src/classes/Environment.ts
@@ -0,0 +1,19 @@
+export class Environment {
+    public name: string;
+    public url: string;
+    public user: string;
+    public password: string;
+    public token: string;
+
+    private file: Interface.Environment.EnvironmentArgs = {};
+
+    constructor(args: Interface.Environment.EnvironmentArgs = {}) {
+        if (args.data) {
+            this.name = args.data.name;
+            this.url = args.data.url;
+            this.user = args.data.user;
+            this.password = args.data.password;
+            this.token = args.data.token;
+        }
+    }
+}
---
+++ src/interface/environment.ts
@@ -0,0 +1,12 @@
+
+
+export interface EnvironmentArgs {
+    // options?: any,
+    data?: {
+        name?: string,
+        url?: string,
+        user?: string,
+        password?: string,
+        token?: string
+    }
+}
